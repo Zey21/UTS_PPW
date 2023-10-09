f41902f126a81b8b7c37a8971c3825b270086a5b
Mukhammad Zaidan Abdulloh <77796843+Zey21@users.noreply.github.com>
2023-10-10 05:57:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,46 @@ import numpy as np
 st.header("UTS PPW")
 st.subheader("Mengambil Data CSV pada Github")
 st.write("**pada repo Zey21/dataset/**")
-st.text("load data")
+st.text("load data(DataStemming.csv) csv yang sudah berhasil di stemming")
 
 ##Load data
 df = pd.read_csv('https://raw.githubusercontent.com/Zey21/dataset/main/DataSteaming.csv')
 out_df = df.head()
 st.text(out_df)
+
+#Ektraksi Fitur
+from sklearn.feature_extraction.text import CountVectorizer
+count_vectorizer = CountVectorizer()
+
+# Gantilah nilai NaN dalam kolom 'Abstrak' dengan string kosong
+df['Abstrak'].fillna('', inplace=True)
+
+X_count = count_vectorizer.fit_transform(np.array(df['Abstrak']))
+
+terms_count = count_vectorizer.get_feature_names_out()
+df_countvect = pd.DataFrame(data = X_count.toarray(),columns = terms_count)
+df_countvect
+
+##LDA Modelling
+from sklearn.decomposition import LatentDirichletAllocation, TruncatedSVD
+
+lda_model = LatentDirichletAllocation(n_components=3, doc_topic_prior=0.1, topic_word_prior=0.2, random_state=42)
+lda_model.fit(df_countvect)
+
+doc_topic_proportions = lda_model.transform(df_countvect)
+
+for i, doc in enumerate(df['Abstrak']):
+    st.text(f"Dokumen {i+1}:")
+    for j, topic_prob in enumerate(doc_topic_proportions[i]):
+        st.text(f"Topik {j+1}: {topic_prob:.4f}")
+    st.text("")
+
+topic_word_distributions = lda_model.components_
+
+feature_names = vectorizer.get_feature_names_out()
+for topic_idx, topic in enumerate(topic_word_distributions):
+    top_words_idx = topic.argsort()[::-1][:10]  # Ambil 10 kata teratas
+    top_words = [feature_names[i] for i in top_words_idx]
+    st.text(f"Topik {topic_idx+1}:")
+    st.text(", ".join(top_words))
+    st.text("")
