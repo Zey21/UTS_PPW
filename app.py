8653aa76b9bbb60909676e030f0ea24f1a8ccbc1
Mukhammad Zaidan Abdulloh <77796843+Zey21@users.noreply.github.com>
2023-10-10 05:52:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.header("UTS PPW")
 st.subheader("Mengambil Data CSV pada Github")
 st.write("**pada repo Zey21/dataset/**")
 st.text("load data")
+
+##Load data
+df = pd.read_csv('https://raw.githubusercontent.com/Zey21/dataset/main/DataSteaming.csv')
+out_df = df.head()
+st.text(out_df)
