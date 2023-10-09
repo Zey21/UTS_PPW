ba1822413fe8956f23b08d290e3e73f5e83b31c5
Mukhammad Zaidan Abdulloh <77796843+Zey21@users.noreply.github.com>
2023-10-10 06:26:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ df.head()
 st.text("Ektraksi Fitur")
 df['Abstrak']
 
-
 st.text("Term Frekuensi")
 from sklearn.feature_extraction.text import CountVectorizer
 count_vectorizer = CountVectorizer()
@@ -25,15 +24,12 @@ df['Abstrak'].fillna('', inplace=True)
 X_count = count_vectorizer.fit_transform(np.array(df['Abstrak']))
 
 terms_count = count_vectorizer.get_feature_names_out()
-df_countvect = pd.DataFrame(data = X_count.toarray(),columns = terms_count)
+df_countvect = pd.DataFrame(data=X_count.toarray(), columns=terms_count)
 df_countvect
 
 token_counts = df_countvect.sum(axis=0)
 
 non_zero_token_counts = token_counts[token_counts != 0]
-
-# print("Token Counts yang Tidak Sama dengan 0:")
-# print(non_zero_token_counts)
 
 st.text("One Hot Encoding")
 df_binary = df_countvect.applymap(lambda x: 1 if x > 0 else 0)
@@ -45,7 +41,7 @@ vectorizer = TfidfVectorizer()
 X_tfidf = vectorizer.fit_transform(df['Abstrak'].tolist())
 
 terms = vectorizer.get_feature_names_out()
-df_tfidfvect = pd.DataFrame(data = X_tfidf.toarray(),columns = terms)
+df_tfidfvect = pd.DataFrame(data=X_tfidf.toarray(), columns=terms)
 df_tfidfvect
 
 st.text("Log Frekuensi")
@@ -58,15 +54,11 @@ from sklearn.decomposition import LatentDirichletAllocation, TruncatedSVD
 lda_model = LatentDirichletAllocation(n_components=3, doc_topic_prior=0.1, topic_word_prior=0.2, random_state=42)
 lda_model.fit(df_countvect)
 
-lda_model
-
 doc_topic_proportions = lda_model.transform(df_countvect)
 
 for i, doc in enumerate(df['Abstrak']):
-    # print(f"Dokumen {i+1}:")
     for j, topic_prob in enumerate(doc_topic_proportions[i]):
-        # print(f"Topik {j+1}: {topic_prob:.4f}")
-    # print()
+        st.text(f"Dokumen {i+1}, Topik {j+1}: {topic_prob:.4f}")
 
 topic_word_distributions = lda_model.components()
 
